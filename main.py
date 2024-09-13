3eac65846194ea0378381a5e67cb45ce5178f625
ClarkeEl360 <ClarkeEl360@northwested.org>
2024-09-13 10:45:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,36 +6,41 @@
 # Comment
 # Comment
 
-
-
+print("Whats your name user?")
+pname = input()
+print("And what do you do at CTC?")
+ctc = input()
+print(f"Hi {pname} its good to have you here at CTC doing {ctc}.")
 
 # Task 2
 # Comment
 # Comment
 
-
-
+print("How old are you?")
+age = int(input())
+agge = int(age + 3)
+print(f"{age} rotations, good for you, only {agge} left.")
 
 # Task 3
 # Comment
 # Comment
 
-
-
+print("How many people are in your familay?")
+fam = int(input())
+print(f"Only {fam} familay members, couldent be me. 583924.6")
 
 # Task 4
 # Comment
 # Comment
 
-
-
-
+print("Whats your hight in inches")
+inch = int(input())
+cm = float(inch * 2.54)
+print(f"You are {cm} cm tall.")
 
 # Task 5
 # Comment
 # Comment
-
-
 
 
 
